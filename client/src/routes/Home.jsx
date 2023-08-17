e0377e42b960db2af0e8a592fc4ea54721b43e27
conorv <70652079+conorvenus@users.noreply.github.com>
2023-08-17 21:36:29 +0100

    On XS screens, collapse the sentiment scores to column

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -29,7 +29,7 @@ export default function Home() {
                     <textarea value={text} onChange={event => setText(event.target.value)} type="username" name="username" id="username" className="bg-slate-800 border-slate-700 border text-slate-400 placeholder:text-slate-400 text-lg rounded-lg block w-full p-2.5 min-h-[200px] focus:outline-slate-500 focus:outline" required />
                 </div>
                 <button type="submit" className="bg-slate-900 rounded-lg py-2 border-slate-800 border">🥳 Analyse Sentiment</button>
-                <div className="flex gap-8">
+                <div className="flex gap-8 max-w-xs:flex-col">
                     <article className="bg-slate-800/50 border border-slate-700/50 flex flex-col items-center py-4 rounded-md flex-1 gap-4">
                         <h1 className="text-7xl">😀</h1>
                         <div className="text-center">
