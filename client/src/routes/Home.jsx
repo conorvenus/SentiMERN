346b6ff855769ede27a144440297f8ca0fd3ccc2
conorv <70652079+conorvenus@users.noreply.github.com>
2023-08-17 13:49:10 +0100

    Added JWT validation on the prediction endpoint

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,13 +1,27 @@
 import { RequireAuth } from "react-auth-kit"
-import { Link } from "react-router-dom"
+import { useState } from "react"
 
 export default function Home() {
+    const [text, setText] = useState("")
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        const response = await fetch(`/api/predict/${text}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await response.json()
+        console.log(data)
+    }
+
     return (
         <RequireAuth loginPath="/login">
-            <form className="py-16 max-w-2xl mx-auto flex-col flex gap-8">
+            <form className="py-16 max-w-2xl mx-auto flex-col flex gap-8" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="text" className="block mb-2 text-sm font-medium text-slate-200">Text</label>
-                    <textarea type="username" name="username" id="username" className="bg-slate-800 border-slate-700 border text-slate-400 placeholder:text-slate-400 text-lg rounded-lg block w-full p-2.5 min-h-[200px] focus:outline-slate-500 focus:outline" required />
+                    <textarea value={text} onChange={event => setText(event.target.value)} type="username" name="username" id="username" className="bg-slate-800 border-slate-700 border text-slate-400 placeholder:text-slate-400 text-lg rounded-lg block w-full p-2.5 min-h-[200px] focus:outline-slate-500 focus:outline" required />
                 </div>
                 <button type="submit" className="bg-slate-900 rounded-lg py-2 border-slate-800 border">🥳 Analyse Sentiment</button>
                 <div className="flex gap-8">
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser")
 require("dotenv").config()
 
 mongoose
@@ -8,8 +10,19 @@ mongoose
     .then(() => console.log("Connected to database..."))
 
 app.use(express.json())
+app.use(cookieParser())
 app.use('/api/auth', require("./endpoints/auth/authRouter"))
 app.use('/api/predict/:text', async (req, res) => {
+    if (!req.cookies || !req.cookies._auth) return res.status(401).json({ message: "Unauthorized" })
+
+    const token = req.cookies._auth
+
+    try {
+        jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(401).json({ message: "Unauthorized" })
+    }
+
     const text = req.params.text
     try {
         const response = await fetch(`http://127.0.0.1:8000/api/predict/${text}`, {
